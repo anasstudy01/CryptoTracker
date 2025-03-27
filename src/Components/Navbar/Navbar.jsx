c0ee4d0103a224874f75c5728512800d676b0bd5
anas <anasstudy01@gmail.com>
2025-03-27 06:37:00 +0530

    implement lazy loading for Home and CoinDetailsPage components

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,19 @@
 import { useContext } from "react";
 import globalscope  from "../../GlobalContext/context";
+import { useNavigate } from "react-router-dom";
 
 function Navbar(){
 
 const {setCurrency}=useContext(globalscope);
+const navigate=useNavigate();
+const goToHome=()=>{
+navigate('/');
+}
 
     return (<>
 
 <div className="navbar bg-base-100 shadow-sm">
-  <div className="navbar-start">
+  <div className="navbar-start" onClick={goToHome} >
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
@@ -23,7 +28,9 @@ const {setCurrency}=useContext(globalscope);
       </ul>
     </div>
   </div>
-  <div className="navbar-center">
+  
+  <div onClick={goToHome} className="navbar-center">
+  
     <a className="btn btn-ghost text-3xl">CoinMarket</a>
   </div>
   <div className="navbar-end">
--- src/Components/Routing/Routing.jsx
+++ src/Components/Routing/Routing.jsx
@@ -1,15 +1,31 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "../../Home";
-import CoinDetailsPage from "../../pages/CoinDetailsPage";
+// import Home from "../../Home";
+// import CoinDetailsPage from "../../pages/CoinDetailsPage";
 import MainLayout from "../../pages/Layout";
+import { lazy, Suspense } from "react";
+
+const Home = lazy(() => import("../../Home"));
+const CoinDetailsPage = lazy(() => import("../../pages/CoinDetailsPage"));
 
 function Routing() {
   return (
     <>
       <Routes>
         <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} /> 
-          <Route path="/details/:coinid" element={<CoinDetailsPage />} />
+          <Route index element={
+              <Suspense fallback={
+                  <div><h1>New Content is on the way...</h1></div> }>
+                <Home />
+              </Suspense>
+            }
+          />
+          <Route    path="/details/:coinid" element={
+              <Suspense fallback={
+                  <div>    <h1>New Content is on the way keep waiting...</h1> </div>  }>
+                <CoinDetailsPage />
+                 </Suspense>
+            }
+          />
         </Route>
       </Routes>
     </>
