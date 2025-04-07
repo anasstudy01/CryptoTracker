b7d71413bd75c885dbeedabb0330f9dd5bd8747e
anas <anasstudy01@gmail.com>
2025-04-08 04:23:12 +0530

    fix: correct import path for Routing component to match folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import GlobalContext from "./GlobalContext/context"; //used alias name for meaingful name
 import { useState } from "react";
-import Routing from "./components/routing/Routing"; // Corrected the path to match the actual folder and file names
+import Routing from "./Components/Routing/Routing"; // Corrected the path to match the actual folder and file names
 
 import './App.css'
 
